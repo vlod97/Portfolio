c83ce74347d575bd26cd7a2bb217d58eb3f0b027
vlod97 <vova2306@gmail.com>
2018-04-12 20:39:29 +0300

    Added automatic profile img slider

---
+++ js/profileSlider.js
@@ -0,0 +1,10 @@
+var current = 0,
+  slides = document.getElementsByTagName("img");
+
+setInterval(function () {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0;
+  }
+  current = (current != slides.length - 1) ? current + 1 : 0;
+  slides[current].style.opacity = 1;
+}, 2500);
